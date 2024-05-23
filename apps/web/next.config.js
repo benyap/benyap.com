b33d7d77730f4387d15eed29bc9ab28836ccbdce
Ben Yap <contact@benyap.com>
2024-05-23 22:43:45 +1000

    chore: reset

--- apps/web/next.config.js
+++
@@ -1,7 +0,0 @@
-const withTM = require("next-transpile-modules")(["ui"]);
-
-module.exports = withTM({
-  // FIXME: set this to true once @headlessui/react Transition component is compatible with React 18
-  // See https://github.com/tailwindlabs/headlessui/issues/681
-  reactStrictMode: false,
-});
