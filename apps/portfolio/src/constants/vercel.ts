cc7236a7f18405e7d8309358ebfdb9d8c7a16943
Ben Yap <contact@benyap.com>
2025-03-31 23:53:05 +1100

    feat(portfolio): add robots.txt and og metadata

--- apps/portfolio/src/constants/vercel.ts
+++ apps/portfolio/src/constants/vercel.ts
@@ -4,3 +4,7 @@ export const VERCEL_ENV = process.env["VERCEL_ENV"];
 
 export const VERCEL_PROJECT_PRODUCTION_URL =
   process.env["VERCEL_PROJECT_PRODUCTION_URL"];
+
+export function isProduction() {
+  return VERCEL_ENV === "production";
+}
