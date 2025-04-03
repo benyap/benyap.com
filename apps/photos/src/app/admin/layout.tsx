9632f26f98a5e2104c9648e6e266e3c984d30afe
Ben Yap <contact@benyap.com>
2025-04-03 14:04:36 +1100

    chore(photos): update font and site meta for photos admin

--- apps/photos/src/app/admin/layout.tsx
+++ apps/photos/src/app/admin/layout.tsx
@@ -1,28 +1,62 @@
+import clsx from "clsx";
 import { Metadata } from "next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Newsreader, Geist_Mono, Inter } from "next/font/google";
+
+import { DESCRIPTION_ADMIN, SITE_NAME_ADMIN } from "~/constants/metadata";
+import { VERCEL_PROJECT_PRODUCTION_DOMAIN } from "~/constants/vercel";
 
 import { AppHeader } from "~/components/admin/AppHeader";
 import { AppSidebar } from "~/components/admin/AppSidebar";
 import { RequireSignIn } from "~/components/auth/RequireSignIn/RequireSignIn";
 import { FirebaseUserProvider } from "~/components/firebase/FirebaseUserProvider";
+import { FirebaseProvider } from "~/components/firebase/FirebaseProvider";
 import { SidebarProvider } from "~/components/ui/sidebar";
 
+const sans = Inter({ subsets: ["latin"], variable: "--font-sans" });
+const serif = Newsreader({ subsets: ["latin"], variable: "--font-serif" });
+const mono = Geist_Mono({ subsets: ["latin"], variable: "--font-mono" });
+
+import "~/app/globals.css";
+
 export const metadata: Metadata = {
-  title: "Admin",
+  title: {
+    default: SITE_NAME_ADMIN,
+    template: `%s - ${SITE_NAME_ADMIN}`,
+  },
+  description: DESCRIPTION_ADMIN,
+  openGraph: {
+    type: "website",
+    title: SITE_NAME_ADMIN,
+    siteName: SITE_NAME_ADMIN,
+    description: DESCRIPTION_ADMIN,
+    url: `https://${VERCEL_PROJECT_PRODUCTION_DOMAIN}`,
+  },
 };
 
 export default function Layout(props: React.PropsWithChildren) {
   const { children } = props;
   return (
-    <FirebaseUserProvider>
-      <RequireSignIn>
-        <SidebarProvider className="min-w-screen">
-          <AppSidebar />
-          <div className="w-full">
-            <AppHeader />
-            <main className="m-4">{children}</main>
-          </div>
-        </SidebarProvider>
-      </RequireSignIn>
-    </FirebaseUserProvider>
+    <html
+      lang="en"
+      className={clsx(sans.variable, serif.variable, mono.variable)}
+    >
+      <body>
+        <FirebaseProvider>
+          <FirebaseUserProvider>
+            <RequireSignIn>
+              <SidebarProvider className="min-w-screen">
+                <AppSidebar />
+                <div className="w-full">
+                  <AppHeader />
+                  <main className="m-4">{children}</main>
+                </div>
+              </SidebarProvider>
+            </RequireSignIn>
+          </FirebaseUserProvider>
+        </FirebaseProvider>
+        <SpeedInsights />
+      </body>
+    </html>
   );
 }
--- apps/photos/src/constants/metadata.ts
+++ apps/photos/src/constants/metadata.ts
@@ -2,3 +2,7 @@ export const SITE_NAME = "Ben Yap Photos";
 
 export const DESCRIPTION =
   "The beauty of life and creation, captured in a moment.";
+
+export const SITE_NAME_ADMIN = "Ben Yap Photos Admin";
+
+export const DESCRIPTION_ADMIN = "No dodgy people allowed.";
